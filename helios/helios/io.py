52614bbb665715ea4799c6cf2a27b1e7aef0d226
sirfoga <sirfoga@protonmail.com>
2019-03-18 18:15:32 +0100

    add DataSaver

---
+++ helios/helios/io.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+
+""" Saves data from EOS db """
+
+import abc
+import gzip
+import json
+import os
+import uuid
+
+from astraeus.models.astraeus import Astraeus
+
+
+class Hasher:
+    """ Something that hashes something """
+
+    @abc.abstractmethod
+    def hash_key(self, key):
+        return 0
+
+
+class UUIDHasher(Hasher):
+    """ Hashing based on UUID4 """
+
+    def hash_key(self, key):
+        hashed = str(uuid.uuid4())
+        hashed = hashed.replace('-', '')
+        return hashed
+
+
+class DataSaver:
+    def __init__(self, hasher, config):
+        """
+        :param hasher: stuff to hash stuff
+        :param config: {} with options. There MUST be a 'out folder' key
+        """
+
+        self.hasher = hasher
+        self.config = config
+
+    def _get_output_file(self, key, extension):
+        key = self.hasher.hash_key(key)
+        out_folder = self.config['out folder']
+        out_file = key + '.' + extension
+        out_file = os.path.join(out_folder, out_file)
+        if os.path.exists(out_file):
+            return None  # cannot save here
+
+        return out_file
+
+    def save_data(self, data):
+        out_file = self._get_output_file(None, 'data')
+        with open(out_file, 'w') as writer:
+            writer.write(data)
+
+        return out_file
+
+    def store_data(self, data):
+        """
+        :param data: bytes
+        """
+
+        out_file = self._get_output_file(None, 'tar.gz')
+        with gzip.GzipFile(out_file, 'w') as writer:
+            writer.write(data)
+
+        return out_file
+
+
+class JsonDataSaver(DataSaver):
+    def save_json(self, data):
+        json_data = json.dumps(data)
+        return self.save_data(json_data)
+
+    def store_json(self, data):
+        json_data = json.dumps(data)
+        json_bytes = json_data.encode('utf-8')
+        return self.store_data(json_bytes)
+
+
+class AstraeusDataSaver(JsonDataSaver):
+    def download_json(self, data):
+        out_file = self.store_json(data)
+        astraeus = Astraeus()
+        download_key = astraeus.save(out_file)  # save real path
+        root_url = self.config['out url']
+        full_url = root_url + download_key
+        return full_url
